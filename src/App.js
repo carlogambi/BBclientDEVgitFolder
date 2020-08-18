8cdcfc3e89e49e024c1ce6db73036003afc9fda1
carlogambi <carlogambi@hotmail.it>
2020-08-18 10:53:39 +0200

    upload bb client dev 18 08 2020 n6

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/header';
+import PageBody from './components/page-body';
+import Footer from './components/footer';
+import langmanager from './langs/langManager';
+
+import $ from 'jquery';
+
+import changeLangEvent from './custom-events/changeLangEvent'
+
+$(document).ready(() => {
+  $('.app').animate({opacity: '1.0'}, 1000);
+});
+export default class App extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      currentLang: langmanager.getDefaultLang(),
+      langPack: langmanager.getLangPack(langmanager.getDefaultLang()),
+      othersLangsList: langmanager.getlangListWithoutCurrent()
+    }
+  }
+
+  updateLangContext(){
+    let currentLang = langmanager.getCurrentLang();
+    let currentLangPack = langmanager.getLangPack(currentLang);
+    let othersLangsList = langmanager.getlangListWithoutCurrent()
+    this.setState({
+      currentLang: currentLang,
+      langPack: currentLangPack,
+      othersLangsList: othersLangsList
+    });
+  }
+
+
+  componentDidMount(){
+    changeLangEvent.intereceptChangeLangEvent(() => {
+      this.updateLangContext();
+    });
+
+  }
+
+  render(){
+    return (
+     <div className="app">
+       <Header langData = {
+         {
+           areaR: this.state.langPack.areaRes, 
+           othersL: this.state.othersLangsList
+           }
+        } />
+       <PageBody data ={
+         {
+           vociMenu: this.state.langPack.vociMenu,
+         }
+       } 
+          footerData = {
+        { 
+          footerInfo: this.state.langPack.footerInfo, 
+          vociFooter: this.state.langPack.vociFooter
+        }
+       }
+       />
+       <Footer
+        isFirstPage={true}
+        data= {
+          { 
+            footerInfo: this.state.langPack.footerInfo, 
+            vociFooter: this.state.langPack.vociFooter
+          }
+        } />
+      </div>
+    );
+  }
 }
-
-export default App;
