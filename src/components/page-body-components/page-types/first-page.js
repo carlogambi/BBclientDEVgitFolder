e5b7624708a0675b15243919006bf2c14e903711
carlogambi <carlogambi@hotmail.it>
2020-08-25 14:30:26 +0200

    upload n1 25-08-2020

--- src/components/page-body-components/page-types/first-page.js
+++ src/components/page-body-components/page-types/first-page.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../../../styles-components/page-body-components/page-types/first-page.css'
 import videoPrimaPagina from './../../../images/video-bb-prima-pagina.mp4'
+import trasformiamoIdeeProdotti from './../../../images/trasformiamo_idee_prodotti.svg'
 
 export default (props) => {
     return <div className="first-page">
-        <h1><i><span class="fondoOp" > &nbsp;trasformiamo&nbsp;</span></i><br></br> <span class="fondoOp" > <i>&nbsp;le </i><span class="someBold">idee&nbsp;</span></span> <br></br><span class="fondoOp" > <i>&nbsp;in </i><span class="someBold">prodotti&nbsp;</span></span></h1>
         <video id="video-prima-pagina" autoPlay loop muted>
             <source src={videoPrimaPagina} />
         </video>
+        <img id="trasformiamo-idee-prodotti" src={trasformiamoIdeeProdotti} alt="trasformiamo le ideee in prodotti"/>
     </div>
 }
