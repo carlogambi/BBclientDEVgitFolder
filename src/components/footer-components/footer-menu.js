55da416d52acc196576d5ce9458cf452a83f3148
carlogambi <carlogambi@hotmail.it>
2020-09-11 13:45:36 +0200

    11/9/2020, prima versione nuova gestione in visible-in-scroll.js: invertita la ricerca dell'elemento visibile: ora è quello piu in alto, prima era quello piu in basso

--- src/components/footer-components/footer-menu.js
+++ src/components/footer-components/footer-menu.js
@@ -10,7 +10,10 @@ export default (props) => {
     const digested = props.vociFooter.map(
         (v, i, a) => {
             if(i < a.length -1){
-                return <React.Fragment key={'frag'+i} ><VoceFooterMenu key={'separator'+i} voce={v.voce} /><label>|</label></React.Fragment>
+                return <React.Fragment key={'frag'+i} >
+                            <VoceFooterMenu key={'separator'+i} voce={v.voce} />
+                            <label>|</label>
+                        </React.Fragment>
             }else{
                 return <VoceFooterMenu key={i} voce={v.voce} />}
             }
--- src/components/util/visible-in-scroll.js
+++ src/components/util/visible-in-scroll.js
@@ -6,55 +6,48 @@ let currentVisible; //string: html node id
 
 let currentVisibleTitle; //html node
 
-export default (targetId, containerId) => {
 
-    const visibleY = (el) => { //input param -el-: html node
-        //initialization
-        let rect = el.getBoundingClientRect(); // object (with position & dimensions): { height: int, width: int, top: int, bottom: int, left: int, right: int, x: int, y: int }
-        let top = rect.top, height = rect.height; 
-        el = el.parentNode; // html node: element id -> #main-container
-        
-        do {
-          rect = el.getBoundingClientRect();
-          if (top <= rect.bottom ) {
-             if(rect.bottom === false){
-               return false;
-              }
-            }
-          // Check if the element is out of view due to a container scrolling
-          if ((top + height) <= rect.top) return false
-          el = el.parentNode;
-        } while (el !== document.body);
-        // Check its within the document viewport
-        return top <= document.documentElement.clientHeight;
-      };
-      
-      // Stuff only for the demo
-      function attachEvent(element, event, callbackFunction) {
-          if (element.addEventListener) {
-              element.addEventListener(event, callbackFunction, false);
-          } else if (element.attachEvent) {
-              element.attachEvent('on' + event, callbackFunction);
-          }
-      };
-      
-      const update = () => {
-          let current = document.getElementById(targetId)
-          if(current){
-            let isVisible = visibleY(current);
-              if(isVisible && (currentVisible !== targetId)){ 
+export default (targetId, containerId) => {
+          document.querySelector('#'+containerId).addEventListener('scroll', () => {
+            let current = document.getElementById(targetId)
+            if(current){
+              let isVisible = visibleY(current);
+              if(isVisible){console.log(document.getElementById(targetId).childNodes[0].textContent);}
+                if(isVisible && (currentVisible !== targetId)){ 
                   currentVisible = targetId;
                   currentVisibleTitle = document.getElementById(targetId).childNodes[0].textContent;
-                  if(scrollAnimationEndIndicator.isAnimationEnded()){
-                    visiblePageSectionEvent.trigger(currentVisibleTitle, {currentVisible})
-                  }
-              }
-          }
-        };
-        
-          attachEvent(document.getElementById(containerId), "scroll", update);
-          attachEvent(window, "resize", update);
-          update();
-         
+                    if(scrollAnimationEndIndicator.isAnimationEnded()){
+                      visiblePageSectionEvent.trigger(currentVisibleTitle, {currentVisible})
+                    }
+                }
+            }
+          });
 
 }
+
+
+
+function visibleY(el) { //input param -el-: html node
+  //initialization
+  let rect = el.getBoundingClientRect(); // object (with node's position & dimensions): 
+  //{ height: int, width: int, top: int, bottom: int, left: int, right: int, x: int, y: int }
+  let top = rect.top;
+  el = el.parentNode; // html node: element id -> #main-container
+  
+  do {
+    rect = el.getBoundingClientRect(); //update position
+    if (top <= rect.bottom ) {
+       if(rect.bottom === false){
+         return false;
+        }
+      }
+
+    // Check if the element is out of view due to a container scrolling
+    if (top  >= rect.top){ return false}
+    el = el.parentNode;
+  } while (el !== document.body);
+  
+  // Check its within the document viewport
+  return top <= document.documentElement.clientHeight;
+};
+
