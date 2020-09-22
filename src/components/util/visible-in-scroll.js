e13b9adbc1a21c5d2c6ab628539aeee010d48f8a
carlogambi <carlogambi@hotmail.it>
2020-09-22 14:23:53 +0200

    22 9 2020 finalmente una scrollTracer che funziona

--- src/components/util/visible-in-scroll.js
+++ src/components/util/visible-in-scroll.js
@@ -1,53 +1,24 @@
 import visiblePageSectionEvent from '../../custom-events/visiblePageSectionEvent'
 
-import scrollAnimationEndIndicator from './scroll-animation-end-indicator';
-
-let currentVisible; //string: html node id 
-
-let currentVisibleTitle; //html node
-
-
+import $ from 'jquery'
 export default (targetId, containerId) => {
-          document.querySelector('#'+containerId).addEventListener('scroll', () => {
-            let current = document.getElementById(targetId)
-            if(current){
-              let isVisible = visibleY(current);
-              if(isVisible){console.log(document.getElementById(targetId).childNodes[0].textContent);}
-                if(isVisible && (currentVisible !== targetId)){ 
-                  currentVisible = targetId;
-                  currentVisibleTitle = document.getElementById(targetId).childNodes[0].textContent;
-                    if(scrollAnimationEndIndicator.isAnimationEnded()){
-                      visiblePageSectionEvent.trigger(currentVisibleTitle, {currentVisible})
-                    }
-                }
-            }
-          });
+  const container= document.querySelector('#'+containerId);
+  const containerRect = document.getElementById(containerId).getBoundingClientRect()
+  const containerheight = containerRect.height;
+  function scrollManager(){
 
+    const headerheight = $('.header').css('height').replace('px', '')    
+    const elementTag = document.getElementById(targetId).firstChild
+    const element = document.getElementById(targetId).firstChild.getBoundingClientRect()
+    const topel = element.top ;
+    let isVisible = (topel -10 > headerheight) && (topel < containerheight/3) ;
+
+    if( isVisible){
+      visiblePageSectionEvent.trigger(elementTag.textContent, {targetId})
+    }
+    
+  };
+  
+  container.removeEventListener('scroll', scrollManager, false);
+  container.addEventListener('scroll', scrollManager, false);
 }
-
-
-
-function visibleY(el) { //input param -el-: html node
-  //initialization
-  let rect = el.getBoundingClientRect(); // object (with node's position & dimensions): 
-  //{ height: int, width: int, top: int, bottom: int, left: int, right: int, x: int, y: int }
-  let top = rect.top;
-  el = el.parentNode; // html node: element id -> #main-container
-  
-  do {
-    rect = el.getBoundingClientRect(); //update position
-    if (top <= rect.bottom ) {
-       if(rect.bottom === false){
-         return false;
-        }
-      }
-
-    // Check if the element is out of view due to a container scrolling
-    if (top  >= rect.top){ return false}
-    el = el.parentNode;
-  } while (el !== document.body);
-  
-  // Check its within the document viewport
-  return top <= document.documentElement.clientHeight;
-};
-
