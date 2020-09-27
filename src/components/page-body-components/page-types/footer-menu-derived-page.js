08c2784ba2ae4b605db5b04a357317968e2f3f45
carlogambi <carlogambi@hotmail.it>
2020-09-27 21:07:14 +0200

    sostituiti tutti i componenti a classe con componenti funzionali con gli hook in previsione delle prossime versioni di React, 27 09 2020

--- src/components/page-body-components/page-types/footer-menu-derived-page.js
+++ src/components/page-body-components/page-types/footer-menu-derived-page.js
@@ -1,61 +1,79 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 import './../../../styles-components/page-body-components/page-types/footer-menu-derived-page.css'
 
-export default class extends React.Component{
+export default (props) => {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            props: props
-        }
-    }
-
-    title(){
-        if(this.state.props.page.voce){ 
-            return <h1 className="f-d-p-title" >{this.state.props.page.voce}</h1>
-        }
-    }
-    subTitle(){
-        if(this.state.props.page.subTitle){ 
-            return <h4 className="f-d-p-subtitle" dangerouslySetInnerHTML={{__html: this.state.props.page.subTitle}}></h4>
-        }
-    }
-
-    mainContent(){
-        if(this.state.props.page.mainContent){ 
-            return <div className="f-d-p-main-content" dangerouslySetInnerHTML={{__html: this.state.props.page.mainContent}}></div>
-        }
-    }
-    footer(){
-        if(this.state.props.page.footer){ 
-            return <div className="f-d-p-footer" dangerouslySetInnerHTML={{__html: this.state.props.page.footer}}></div>
-        }
-    }
-
-
-    componentDidUpdate(){
+    useEffect(() => {
         document.getElementsByClassName('main-container')[0].scrollTo({
             top: 0,
             left: 0,
             behavior: 'smooth'
         });
-    }
+    })
 
-    componentWillReceiveProps(props){
-        this.setState({
-            props: props
-        })
-        this.forceUpdate();
-    }
-
-    render(){
-        return <div className="footer-derived-page">
-        {this.title()}
-        {this.subTitle()}
-  
-        {this.mainContent()}
-        {this.footer()}
-        </div>
-    }
+    return <div className="footer-derived-page">
+    {props.page.voce?<h1 className="f-d-p-title" >{props.page.voce}</h1>:null}        
+    {props.page.subTitle?<h4 className="f-d-p-subtitle" dangerouslySetInnerHTML={{__html: props.page.subTitle}}></h4>:null}        
+    {props.page.mainContent?<div className="f-d-p-main-content" dangerouslySetInnerHTML={{__html: props.page.mainContent}}></div>:null}        
+    {props.page.footer?<div className="f-d-p-footer" dangerouslySetInnerHTML={{__html: props.page.footer}}></div>:null}        
+    </div>
 }
+
+// export default class extends React.Component{
+
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             props: props
+//         }
+//     }
+
+//     title(){
+//         if(this.state.props.page.voce){ 
+//             return <h1 className="f-d-p-title" >{this.state.props.page.voce}</h1>
+//         }
+//     }
+//     subTitle(){
+//         if(this.state.props.page.subTitle){ 
+//             return <h4 className="f-d-p-subtitle" dangerouslySetInnerHTML={{__html: this.state.props.page.subTitle}}></h4>
+//         }
+//     }
+
+//     mainContent(){
+//         if(this.state.props.page.mainContent){ 
+//             return <div className="f-d-p-main-content" dangerouslySetInnerHTML={{__html: this.state.props.page.mainContent}}></div>
+//         }
+//     }
+//     footer(){
+//         if(this.state.props.page.footer){ 
+//             return <div className="f-d-p-footer" dangerouslySetInnerHTML={{__html: this.state.props.page.footer}}></div>
+//         }
+//     }
+
+
+//     componentDidUpdate(){
+//         document.getElementsByClassName('main-container')[0].scrollTo({
+//             top: 0,
+//             left: 0,
+//             behavior: 'smooth'
+//         });
+//     }
+
+//     componentWillReceiveProps(props){
+//         this.setState({
+//             props: props
+//         })
+//         this.forceUpdate();
+//     }
+
+//     render(){
+//         return <div className="footer-derived-page">
+//         {this.title()}
+//         {this.subTitle()}
+  
+//         {this.mainContent()}
+//         {this.footer()}
+//         </div>
+//     }
+// }
--- src/components/page-body-components/page-types/second-level-page-main-menu.js
+++ src/components/page-body-components/page-types/second-level-page-main-menu.js
@@ -1,18 +1,11 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './../../../styles-components/page-body-components/page-types/second-level-page-main-menu.css';
 
 import $ from 'jquery'
 
-export default class SlPage extends React.Component{
+export default  (props) => {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            props
-        }
-    }
-
-    manageVideoLoad(){
+    const manageVideoLoad = () => {
         $('.s-l-mainContent video').css({opacity: 0});
         $('.s-l-mainContent video').each((i, video) => {
             $(video).on('canplay', () => {
@@ -23,7 +16,7 @@ export default class SlPage extends React.Component{
         })
     }
 
-    manageImgLoad(){
+    const manageImgLoad = () => {
         $('.s-l-page img').css({opacity: 0});
         $('.s-l-page img').each((i, img) => {
             $(img).ready(() => {
@@ -34,42 +27,86 @@ export default class SlPage extends React.Component{
         })
     }
 
-    componentDidUpdate(){
-        this.manageVideoLoad();
-    }
-    
-    componentDidMount(){
-        this.manageVideoLoad();        
-    }
+    useEffect(() => {
+        manageVideoLoad()
+        manageImgLoad()
+    })
 
-    title(){
-        if(this.props.page.voce){ 
-            return <h1 className="s-l-title">{this.props.page.voce}</h1>
-        }
-    }
-    subTitle(){
-        if(this.props.page.subTitle){ 
-            return <h4 className="s-l-subTitle"  dangerouslySetInnerHTML={{__html: this.props.page.subTitle}}></h4>
-        }
-    }
-
-    mainContent(){
-        if(this.props.page.mainContent){ 
-            return <div className="s-l-mainContent" dangerouslySetInnerHTML={{__html: this.props.page.mainContent}} />;
-        }
-    }
-    footer(){
-        if(this.props.page.footer){ 
-            return <div className="s-l-footer" dangerouslySetInnerHTML={{__html: this.props.page.footer}} />;
-        }
-    }
-
-    render(){
-        return <div className="s-l-page" id={this.props.id} data-voce-ref={this.props.page.voce}>
-            {this.title()}
-            {this.subTitle()}
-            {this.mainContent()}
-            {this.footer()}
-        </div>
-    }
+    return <div className="s-l-page" id={props.id} data-voce-ref={props.page.voce}>
+    {props.page.voce?<h1 className="s-l-title" >{props.page.voce}</h1>:null}        
+    {props.page.subTitle?<h4 className="s-l-subtitle" dangerouslySetInnerHTML={{__html: props.page.subTitle}}></h4>:null}        
+    {props.page.mainContent?<div className="s-l-main-content" dangerouslySetInnerHTML={{__html: props.page.mainContent}}></div>:null}        
+    {props.page.footer?<div className="s-l-footer" dangerouslySetInnerHTML={{__html: props.page.footer}}></div>:null}        
+    </div>
 }
+
+// export default class SlPage extends React.Component{
+
+//     constructor(props){
+//         super(props);
+//         this.state = {
+//             props
+//         }
+//     }
+
+//     manageVideoLoad(){
+//         $('.s-l-mainContent video').css({opacity: 0});
+//         $('.s-l-mainContent video').each((i, video) => {
+//             $(video).on('canplay', () => {
+//                 if($(video).css('opacity') === '0'){
+//                     $(video).animate({ opacity: 1 }, 500);
+//                 }
+//             })
+//         })
+//     }
+
+//     manageImgLoad(){
+//         $('.s-l-page img').css({opacity: 0});
+//         $('.s-l-page img').each((i, img) => {
+//             $(img).ready(() => {
+//                 if($(img).css('opacity') === '0'){
+//                     $(img).animate({ opacity: 1 }, 500);
+//                 }
+//             })
+//         })
+//     }
+
+//     componentDidUpdate(){
+//         this.manageVideoLoad();
+//     }
+    
+//     componentDidMount(){
+//         this.manageVideoLoad();        
+//     }
+
+//     title(){
+//         if(this.props.page.voce){ 
+//             return <h1 className="s-l-title">{this.props.page.voce}</h1>
+//         }
+//     }
+//     subTitle(){
+//         if(this.props.page.subTitle){ 
+//             return <h4 className="s-l-subTitle"  dangerouslySetInnerHTML={{__html: this.props.page.subTitle}}></h4>
+//         }
+//     }
+
+//     mainContent(){
+//         if(this.props.page.mainContent){ 
+//             return <div className="s-l-mainContent" dangerouslySetInnerHTML={{__html: this.props.page.mainContent}} />;
+//         }
+//     }
+//     footer(){
+//         if(this.props.page.footer){ 
+//             return <div className="s-l-footer" dangerouslySetInnerHTML={{__html: this.props.page.footer}} />;
+//         }
+//     }
+
+//     render(){
+//         return <div className="s-l-page" id={this.props.id} data-voce-ref={this.props.page.voce}>
+//             {this.title()}
+//             {this.subTitle()}
+//             {this.mainContent()}
+//             {this.footer()}
+//         </div>
+//     }
+// }
